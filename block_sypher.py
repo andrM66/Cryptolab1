abd1841371aaec1a4aa8e72d00352450a83d8ef3
Andrei <marinin1337@gmail.com>
2025-09-25 18:40:26 +0400

    точно все

--- block_sypher.py
+++ block_sypher.py
@@ -1,4 +1,16 @@
 from operator import indexOf
+import random
+
+
+def generate_key(output_filename: str, key_length: int) -> None:
+    key = list(range(0, key_length))
+    key = list(map(str, key))
+    random.shuffle(key)
+    result = " ".join(key)
+    f = open(output_filename, "w")
+    f.write(result)
+
+
 
 
 def cypher_text(input_filename: str, output_filename: str, key_filename) -> None:
@@ -13,16 +25,14 @@ def cypher_text(input_filename: str, output_filename: str, key_filename) -> None
     key_length = len(key)
     result = []
     tmp_text_array = ""
-    tmp_text_array = tmp_text_array.zfill(18)
-    tmp_text_array = tmp_text_array.replace("0", " ")
+    tmp_text_array = tmp_text_array.ljust(key_length)
     tmp_text_array = list(tmp_text_array)
     for i in range(len(open_text)):
         tmp_text_array[key[i%key_length]] = open_text[i]
-        if i % key_length == 17:
+        if i % key_length == key_length - 1:
             result = result + tmp_text_array
-            tmp_text_array = " "
-            tmp_text_array = tmp_text_array.zfill(18)
-            tmp_text_array = tmp_text_array.replace("0", " ")
+            tmp_text_array = ""
+            tmp_text_array = tmp_text_array.ljust(key_length)
             tmp_text_array = list(tmp_text_array)
     result = result + tmp_text_array
     f = open(output_filename, "w", encoding='utf-8')
@@ -43,17 +53,15 @@ def decypher_text(input_filename: str, key_filename: str, output_filename: str) 
     key_length = len(key)
     result = []
     tmp_text_array = ""
-    tmp_text_array = tmp_text_array.zfill(18)
-    tmp_text_array = tmp_text_array.replace("0", " ")
+    tmp_text_array = tmp_text_array.ljust(key_length)
     tmp_text_array = list(tmp_text_array)
     tmp_cyphered_text = [list(cyphered_text[i:i + key_length]) for i in range(0, len(cyphered_text), key_length)]
     for i in range(len(cyphered_text)):
         tmp_text_array[indexOf(key, i % key_length)] = tmp_cyphered_text[i // key_length][ i % key_length]
-        if i % key_length == 17:
+        if i % key_length == key_length - 1:
             result = result + tmp_text_array
-            tmp_text_array = " "
-            tmp_text_array = tmp_text_array.zfill(18)
-            tmp_text_array = tmp_text_array.replace("0", " ")
+            tmp_text_array = ""
+            tmp_text_array = tmp_text_array.ljust(key_length)
             tmp_text_array = list(tmp_text_array)
     result = result + tmp_text_array
     f = open(output_filename, "w", encoding='utf-8')
@@ -64,5 +72,6 @@ def decypher_text(input_filename: str, key_filename: str, output_filename: str) 
 
 
 if __name__ == '__main__':
+    generate_key("cyph_key.txt", 18)
     cypher_text("open_text.txt", "test.txt", "cyph_key.txt")
     decypher_text("test.txt", "cyph_key.txt", "result.txt")
