51d20683b537af82825b13e124d3e630fdee6fca
Andrei <marinin1337@gmail.com>
2025-09-25 17:30:47 +0400

    Initial commit

---
+++ block_sypher.py
@@ -0,0 +1,51 @@
+def cypher_text(input_filename: str, output_filename: str, key_filename) -> None:
+    f = open(input_filename)
+    open_text = f.read()
+    f.close()
+    f = open(key_filename)
+    key = f.read()
+    f.close()
+    key = list(key.split(" "))
+    key = list(map(int, key))
+    key_length = len(key)
+    result = []
+    tmp_text_array = ""
+    tmp_text_array = tmp_text_array.zfill(18)
+    tmp_text_array = tmp_text_array.replace("0", " ")
+    tmp_text_array = list(tmp_text_array)
+    for i in range(len(open_text)):
+        tmp_text_array[key[i%key_length]] = open_text[i]
+        if i % key_length == 17:
+            result = result + tmp_text_array
+            tmp_text_array = " "
+            tmp_text_array = tmp_text_array.zfill(18)
+            tmp_text_array = tmp_text_array.replace("0", " ")
+            tmp_text_array = list(tmp_text_array)
+    result = result + tmp_text_array
+    f = open(output_filename, "w")
+    result = "".join(result)
+    f.write(result)
+    f.close()
+
+
+def decypher_text(input_filename: str, key_filename: str, output_filename: str) -> None:
+    f = open(input_filename)
+    cyphered_text = f.read()
+    f.close()
+    f = open(key_filename)
+    key = f.read()
+    f.close()
+    key = list(key.split(" "))
+    key = list(map(int, key))
+    key_length = len(key)
+    result = []
+    tmp_text_array = ""
+    tmp_text_array = tmp_text_array.zfill(18)
+    tmp_text_array = tmp_text_array.replace("0", " ")
+    tmp_text_array = list(tmp_text_array)
+    #for i in range(len(cyphered_text)):
+
+
+
+if __name__ == '__main__':
+    cypher_text("open_text.txt", "test.txt", "cyph_key.txt")
